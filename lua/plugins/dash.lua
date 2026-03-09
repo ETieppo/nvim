a507b7086e963fe5c67e5cb956510b9da7b834a3
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-03-09 10:19:56 -0300

    fix: dash image path

--- lua/plugins/dash.lua
+++ lua/plugins/dash.lua
@@ -8,7 +8,7 @@ return {
       sections = {
         {
           section = 'terminal',
-          cmd = 'pixterm -s 1 -d 2 -nobg ~/Downloads/craiyon_164341_Skeleton_in_pixelart_with_a_black_fedora__no_eyes__acid_dripping_from_hat.png',
+          cmd = 'pixterm -s 1 -d 2 -nobg ~/.config/nvim/dash.png',
           height = 30,
           padding = 1,
         },
