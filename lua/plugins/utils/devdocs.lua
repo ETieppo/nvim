7ce744ed1e6720b50b697b3d3ff03bd658e65705
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-03-15 18:16:42 -0300

    fix: apidocs ensure installed does not download languages docs properlly, fix it

--- lua/plugins/utils/devdocs.lua
+++ lua/plugins/utils/devdocs.lua
@@ -6,15 +6,21 @@ return {
   },
   cmd = { 'ApidocsSearch', 'ApidocsInstall', 'ApidocsOpen', 'ApidocsSelect', 'ApidocsUninstall' },
   config = function()
-    require('apidocs').setup()
-    require('apidocs').ensure_install {
-      'rust',
-      'angular',
-      'typescript',
-      'openjdk~25',
-      'tailwindcss',
-      'c',
-    }
+    local apidocs = require 'apidocs'
+    apidocs.setup()
+
+    vim.schedule(
+      function()
+        apidocs.ensure_install {
+          'rust',
+          'angular',
+          'typescript',
+          'openjdk~21',
+          'tailwindcss',
+          'c',
+        }
+      end
+    )
   end,
   keys = {
     { '<leader>sad', '<cmd>ApidocsOpen<cr>', desc = 'Search Api Doc' },
