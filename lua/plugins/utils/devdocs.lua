9737774d6bf4659d4218f9a8d58bc899b7d8fc51
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-03-15 17:00:37 -0300

    fix: add ensure_instaled at apidocs for langs

--- lua/plugins/utils/devdocs.lua
+++ lua/plugins/utils/devdocs.lua
@@ -2,14 +2,19 @@ return {
   'emmanueltouzery/apidocs.nvim',
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
-    'nvim-telescope/telescope.nvim', -- or, 'folke/snacks.nvim'
+    'nvim-telescope/telescope.nvim',
   },
   cmd = { 'ApidocsSearch', 'ApidocsInstall', 'ApidocsOpen', 'ApidocsSelect', 'ApidocsUninstall' },
   config = function()
     require('apidocs').setup()
-    -- Picker will be auto-detected. To select a picker of your choice explicitly you can set picker by the configuration option 'picker':
-    -- require('apidocs').setup({picker = "snacks"})
-    -- Possible options are 'ui_select', 'telescope', and 'snacks'
+    require('apidocs').ensure_install {
+      'rust',
+      'angular',
+      'typescript',
+      'openjdk~25',
+      'tailwindcss',
+      'c',
+    }
   end,
   keys = {
     { '<leader>sad', '<cmd>ApidocsOpen<cr>', desc = 'Search Api Doc' },
