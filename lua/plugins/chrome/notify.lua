a5c4d402be61405b6b831bde55167417a3c169a5
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-03-19 15:12:15 -0300

    style: move notfy plugins to chrome

---
+++ lua/plugins/chrome/notify.lua
@@ -0,0 +1,95 @@
+return {
+  {
+    'rcarriga/nvim-notify',
+    lazy = false,
+    config = function()
+      local notify = require 'notify'
+      notify.setup {
+        stages = 'fade',
+        timeout = 5000,
+        render = 'wrapped-compact',
+      }
+      vim.notify = notify
+    end,
+  },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    opts = {
+      messages = { enabled = true },
+      notify = {
+        enabled = true,
+        view = 'notify',
+      },
+
+      cmdline = {
+        enabled = true,
+        format = {
+          cmdline = {
+            pattern = '^:',
+            icon = ' ',
+            lang = 'vim',
+            conceal = false,
+            title=""
+          },
+          search_down = {
+            kind = 'search',
+            pattern = '^/',
+            icon = '  ',
+            lang = 'regex',
+            conceal = false,
+            title=""
+          },
+          search_up = {
+            kind = 'search',
+            pattern = '^%?',
+            icon = '  ',
+            lang = 'regex',
+            conceal = false,
+                        title=""
+
+          },
+          filter = {
+            pattern = '^:%s*!',
+            icon = '$ ',
+            lang = 'bash',
+            conceal = false,
+            title=""
+          },
+          lua = {
+            pattern = {
+              '^:%s*lua%s+',
+              '^:%s*lua%s*=%s*',
+              '^:%s*=%s*',
+            },
+            icon = ' ',
+            lang = 'lua',
+            conceal = false,
+            title=""
+          },
+          help = {
+            pattern = '^:%s*he?l?p?%s+',
+            icon = ' ',
+            conceal = false,
+            title=""
+          },
+          input = {
+            view = 'cmdline_input',
+            icon = '󰥻 ',
+            title=""
+          },
+        },
+      },
+
+      popupmenu = { enabled = true },
+      presets = {
+        long_message_to_split = true,
+        command_palette = true,
+      },
+    },
+  },
+}
