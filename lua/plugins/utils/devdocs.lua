1d7d9b0f76dab17c241676ab4ffae9719ff49445
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-03-19 15:09:53 -0300

    chore: change dev docs plugin, it does't freeze

--- lua/plugins/utils/devdocs.lua
+++ lua/plugins/utils/devdocs.lua
@@ -39,17 +39,29 @@ return {
   },
   opts = {
     ensure_installed = {
-      'go',
       'html',
-      -- "dom",
+      'css',
+      'dom',
       'http',
-      -- "css",
-      -- "javascript",
-      -- "rust",
-      -- some docs such as lua require version number along with the language name
-      -- check `DevDocs install` to view the actual names of the docs
+      'tailwindcss',
+      'angular',
+      'react',
+      'typescript',
+      'c',
+      'rust',
+      'openjdk~25',
+      'postgresql~18',
+      'git',
+      'nextjs',
+      'node',
+      'bash',
+      'bun',
+      'dart~2',
+      'docker',
+      'eslint',
+      'express',
+      'fastapi',
       'lua~5.1',
-      -- "openjdk~21"
     },
   },
 }
