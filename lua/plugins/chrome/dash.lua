e3d6e443496f674f1a7c928b58773f3c986a2ce1
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-03-19 13:02:21 -0300

    chore: remove image filename memory allocation by performance reasons

--- lua/plugins/chrome/dash.lua
+++ lua/plugins/chrome/dash.lua
@@ -2,16 +2,7 @@ math.randomseed(os.time())
 
 local path = vim.fn.expand '~/.config/nvim/assets/'
 local dir_files = vim.fn.readdir(path)
-local images = {}
-local allowed_file_types = { 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp' }
-local function is_allowed_file(fname) return vim.tbl_contains(allowed_file_types, vim.fn.fnamemodify(fname, ':e'):lower()) end
-local wall_selection
-
-for _, f in ipairs(dir_files) do
-  if is_allowed_file(f) then table.insert(images, f) end
-end
-
-wall_selection = images[math.random(1, #images)]
+local wall_selection = dir_files[math.random(2, #dir_files)]
 
 return {
   'folke/snacks.nvim',
@@ -24,7 +15,7 @@ return {
       sections = {
         {
           section = 'terminal',
-          cmd = 'sleep 0.05 && pixterm -d 2 -s 1 ' .. path .. wall_selection,
+          cmd = 'sleep 0.02 && pixterm -d 2 -s 1 ' .. path .. wall_selection,
           width = vim.o.columns,
           height = math.floor(vim.o.lines * 0.75),
           padding = 0,
