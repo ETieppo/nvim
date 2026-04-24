d0d5957a06f9532c6afe6dfd697961b8f23e140f
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-04-24 18:53:14 -0300

    feat: add cursor anim

--- lua/plugins/anim/smear-cursor.lua
+++ lua/plugins/anim/smear-cursor.lua
@@ -2,9 +2,9 @@ return {
   'sphamba/smear-cursor.nvim',
   event = 'VeryLazy',
   opts = {
-    stiffness = 0.8,
-    trailling_stiftness = 0.5,
-    distance_stop_animating = 0.5,
+    -- stiffness = 0.8,
+    -- trailling_stiftness = 0.5,
+    -- distance_stop_animating = 0.5,
     -- cursor_color = '#d3cdc3',
   },
 }
