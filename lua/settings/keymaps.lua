53a83d95f84c6fcd0fe4978891f28b790b369c07
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-05-05 14:33:43 -0300

    style: reorder keymaps

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -17,13 +17,33 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', ';', ':')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Open lazy' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<D-M-Right>', '<cmd>BufferLineCycleNext<CR>', { silent = true, desc = 'Next buffer' })
+vim.keymap.set('n', '<D-M-Left>', '<cmd>BufferLineCyclePrev<CR>', { silent = true, desc = 'Previous buffer' })
+
+if vim.g.neovide then
+  vim.keymap.set('v', '<D-c>', '"+y')
+  vim.keymap.set('n', '<D-v>', '"+P')
+  vim.keymap.set('v', '<D-v>', '"+P')
+  vim.keymap.set('i', '<D-v>', '<C-r>+')
+  vim.keymap.set('c', '<D-v>', '<C-r>+')
+  vim.keymap.set('t', '<D-v>', [[<C-\><C-n>"+pi]])
+  vim.keymap.set('n', '<D-a>', 'ggVG')
+end
+
 vim.keymap.set('n', '<leader>bd', function()
   local cur = vim.api.nvim_get_current_buf()
   local next_buf = nil
   local alt = vim.fn.bufnr '#'
-  if alt > 0 and alt ~= cur and vim.fn.buflisted(alt) == 1 then
-    next_buf = alt
-  end
+  if alt > 0 and alt ~= cur and vim.fn.buflisted(alt) == 1 then next_buf = alt end
   if not next_buf then
     for _, info in ipairs(vim.fn.getbufinfo { buflisted = 1 }) do
       if info.bufnr ~= cur then
@@ -35,28 +55,7 @@ vim.keymap.set('n', '<leader>bd', function()
   if next_buf then vim.api.nvim_set_current_buf(next_buf) end
   vim.cmd('bdelete ' .. cur)
 end, { desc = '[B]uffer [D]elete' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-if vim.g.neovide then
-  vim.keymap.set("v", "<D-c>", '"+y')
-  vim.keymap.set("n", "<D-v>", '"+P')
-  vim.keymap.set("v", "<D-v>", '"+P')
-  vim.keymap.set("i", "<D-v>", '<C-r>+')
-  vim.keymap.set("c", "<D-v>", '<C-r>+')
-  vim.keymap.set("t", "<D-v>", [[<C-\><C-n>"+pi]])
-  vim.keymap.set("n", "<D-a>", "ggVG")
-end
-
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- file
 vim.keymap.set('n', '<leader>fn', function()
   local dir = vim.fn.expand '%:p:h'
   local path = vim.fn.input('New file: ', dir .. '/', 'file')
@@ -106,13 +105,10 @@ vim.keymap.set('n', '<leader>fc', function()
   local old = vim.fn.expand '%:p'
   local name = vim.fn.expand '%:t'
   local move_cursor = #name
-
   vim.schedule(function() vim.api.nvim_feedkeys(string.rep(vim.api.nvim_replace_termcodes('<Left>', true, false, true), move_cursor), 'n', false) end)
-
   local new = vim.fn.input('Copy to: ', old, 'file')
   if new == '' or new == old then return end
   vim.fn.mkdir(vim.fn.fnamemodify(new, ':h'), 'p')
-
   local success = vim.loop.fs_copyfile(old, new)
   if success then
     vim.cmd('edit ' .. vim.fn.fnameescape(new))
@@ -122,29 +118,22 @@ vim.keymap.set('n', '<leader>fc', function()
 end, { desc = '[F]ile [C]opy' })
 
 vim.keymap.set('n', '<leader>cn', function()
-  local config_lua_path = vim.fn.stdpath('config') .. '/lua/'
+  local config_lua_path = vim.fn.stdpath 'config' .. '/lua/'
   local path = vim.fn.input('New Lua config: ', config_lua_path, 'file')
   if path == '' then return end
-  if not path:match('%.lua$') then
-    path = path .. '.lua'
-  end
+  if not path:match '%.lua$' then path = path .. '.lua' end
   vim.fn.mkdir(vim.fn.fnamemodify(path, ':h'), 'p')
   vim.cmd('edit ' .. path)
 end, { desc = '[C]onfig [N]ew' })
 
--- buffer navigation (mirrors Zed alt+super+arrow)
-vim.keymap.set('n', '<D-M-Right>', '<cmd>BufferLineCycleNext<CR>', { silent = true, desc = 'Next buffer' })
-vim.keymap.set('n', '<D-M-Left>', '<cmd>BufferLineCyclePrev<CR>', { silent = true, desc = 'Previous buffer' })
-
-
-vim.keymap.set('n', '<leader>cg', function()
-  require('telescope.builtin').live_grep({
-    cwd = vim.fn.stdpath('config'),
-    prompt_title = 'Live grep in neovim config',
-  })
-end, { desc = '[C]onfig [G]rep' })
-
--- vim.keymap.set({ 'n', 'x' }, 'j', 'gj', { silent = true })
--- vim.keymap.set({ 'n', 'x' }, 'k', 'gk', { silent = true })
--- vim.keymap.set({ 'n', 'x' }, '<Down>', 'gj', { silent = true })
--- vim.keymap.set({ 'n', 'x' }, '<Up>', 'gk', { silent = true })
+vim.keymap.set(
+  'n',
+  '<leader>cg',
+  function()
+    require('telescope.builtin').live_grep {
+      cwd = vim.fn.stdpath 'config',
+      prompt_title = 'Live grep in neovim config',
+    }
+  end,
+  { desc = '[C]onfig [G]rep' }
+)
