ba634d812606821eb96c20ab483be455b2fbc320
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-07-03 16:15:50 -0300

    fix: rust analyzer connection commands, change to right ones

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -1,12 +1,20 @@
 return {
   cmd = { vim.fn.expand '~/.cargo/bin/rust-analyzer' },
   settings = {
-    rust_analyzer= {
-      enable_build_on_save = true,
-      build_on_save_step = 'check',
-      enable_ast_check_diagnostics = true,
-      force_autofix = true,
-      warn_style = true,
+    ['rust-analyzer'] = {
+      checkOnSave = true,
+      check = {
+        command = 'clippy',
+      },
+      procMacro = {
+        enable = true,
+      },
+      cargo = {
+        buildScripts = { enable = true },
+      },
+      diagnostics = {
+        enable = true,
+      },
     },
   },
 }
